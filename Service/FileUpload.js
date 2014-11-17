63917717d7be4117018664df9b5456975f241200
Oleksii Strutsynskyi <cajoy1981@gmail.com>
2014-11-17 14:48:00 -0500

    Added thumb directive

--- Service/FileUpload.js
+++ Service/FileUpload.js
@@ -63,7 +63,9 @@ angular.module('shared')
           console.info('onCompleteItem', fileItem, response, status, headers);
         };
         uploader.onCompleteAll = function() {
-          window.location = options.redirectOnSuccess;
+          if (options.redirectOnSuccess) {
+            window.location = options.redirectOnSuccess;
+          }
         };
       },
       upload: function($scope, targetId) {
@@ -71,12 +73,63 @@ angular.module('shared')
           return false;
         }
 
-        $scope.uploader.formData[0].targetId = targetId;
+        $scope.uploader.formData[0].targetId          = targetId;
         $scope.uploader.queue[0].formData[0].targetId = targetId;
 
         $scope.uploader.uploadAll();
 
         return true;
+      }
+    };
+  }])
+  .directive('ngThumb', ['$window', function($window) {
+    var helper = {
+      support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+      isFile: function(item) {
+        return angular.isObject(item) && item instanceof $window.File;
+      },
+      isImage: function(file) {
+        var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+        return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+      }
+    };
+
+    return {
+      restrict: 'A',
+      template: '<canvas/>',
+      link: function(scope, element, attributes) {
+        if (!helper.support) {
+          return;
+        }
+
+        var params = scope.$eval(attributes.ngThumb);
+
+        if (!helper.isFile(params.file)) {
+          return;
+        }
+        if (!helper.isImage(params.file)) {
+          return;
+        }
+
+        var canvas = element.find('canvas');
+        var reader = new FileReader();
+
+        reader.onload = onLoadFile;
+        reader.readAsDataURL(params.file);
+
+        function onLoadFile(event) {
+          var img = new Image();
+          img.onload = onLoadImage;
+          img.src = event.target.result;
+        }
+
+        function onLoadImage() {
+          var width  = params.width || this.width / this.height * params.height;
+          var height = params.height || this.height / this.width * params.width;
+
+          canvas.attr({ width: width, height: height });
+          canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+        }
       }
     };
   }]);
