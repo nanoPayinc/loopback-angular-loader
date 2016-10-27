1cf2e13ad8a1ee9efa99c0ef78ca82abf95a3703
Joe Auty <joe@datasprocket.com>
2016-10-27 14:39:39 -0400

    oauth2 authorize support

--- Application/Security.js
+++ Application/Security.js
@@ -5,8 +5,8 @@
 angular.module('shared')
 
 .factory(
-  'ApplicationSecurity', ['$rootScope', 'LoopBackAuth', 'User', '$q', '$window', '$cookies',
-  function($rootScope, LoopBackAuth, User, $q, $window, $cookies) {
+  'ApplicationSecurity', ['$rootScope', 'LoopBackAuth', 'User', 'Oauth2', '$q', '$window', '$cookies',
+  function($rootScope, LoopBackAuth, User, Oauth2, $q, $window, $cookies) {
     var currentUser = {};
 
     var internal = {
@@ -98,6 +98,91 @@ angular.module('shared')
           }
         );
       },
+      oAuth2Authorize: function(client_id, redirect_uri, response_type, scope, callback) {
+        var self = this;
+        
+        switch (response_type) {
+        case "code":
+          // authorization code grant
+          
+          // establish user ID
+          
+          Oauth2.authorize({
+            client_id:client_id, 
+            redirect_uri:redirect_uri, 
+            response_type:"code", 
+            scope:scope, 
+            user_id:"57f7fad49dd5d09b4912d60b"
+          }, function(authCode) {
+            //console.log(authCode);
+            if (authCode.authorizationCode) {
+              // exchange auth code for access token, API will auto-supply the client secret
+              Oauth2.token({
+                code:authCode.authorizationCode,
+                client_id:client_id,
+                client_secret:null,
+                grant_type:"authorization_code",
+                redirect_uri:authCode.redirectUri 
+              }, function(accessToken) {
+                console.log(accessToken);
+                
+                accessToken.id = accessToken.accessToken;
+                
+                self.initLogin(accessToken, {}, callback, {
+                  loginRedirect: accessToken.client.redirectUri
+                }, {});
+              }, function(accessTokenError) {
+                console.log(accessTokenError);
+              }); 
+            }
+            else {
+              console.log("Error authorizing application with oAuth2 server");
+            }
+          }, function(authCodeError) {
+            console.log(authCodeError);
+          });
+          break;
+        }
+      },
+      initLogin: function(accessToken, userData, callback, options, user) {
+        LoopBackAuth.isAdmin = user.isAdmin;
+        
+        if (Environment.getConfig('cookieName')) {
+          // set cookie used to keep Loopback access token TTL with frontend in sync
+          var expiration = Date.now() + (accessToken.ttl * 1000);
+          var expirationObj = null;
+          if (userData.rememberme) {
+            // allow cookie to be retained across browser sessions if "remember me" is checked
+            expirationObj = { 
+              expires:new Date(expiration) 
+            }; 
+          }
+        
+          $cookies.putObject(Environment.getConfig('cookieName'), {
+            id:accessToken.id,
+            expiration:expiration
+          }, expirationObj); 
+        }
+        
+        if (typeof options.loginRedirect !== "undefined" && options.loginRedirect === false) {
+          currentUser = user;
+          callback(false, user);
+        }
+        else {
+          if (Environment.getConfig('loginRedirect') && 
+          typeof Environment.getConfig('loginRedirect') === "function") {
+            options.loginRedirect = Environment.getConfig('loginRedirect')(user, $cookies.getObject(Environment.getConfig('cookieName') + '_loginref'));
+            // delete loginref so that it is only used once
+            $cookies.remove(Environment.getConfig('cookieName') + '_loginref');
+          }
+
+          currentUser = user;
+
+          //callback(false, user);
+
+          window.location = options.loginRedirect || Environment.getConfig('loginRedirect');  
+        }
+      },
       login: function(userData, callback, options) {
         if (! options) {
           options = {};
@@ -114,44 +199,7 @@ angular.module('shared')
                 'id' : accessToken.userId
               },
               function(user) {
-                LoopBackAuth.isAdmin = user.isAdmin;
-                
-                if (Environment.getConfig('cookieName')) {
-                  // set cookie used to keep Loopback access token TTL with frontend in sync
-                  var expiration = Date.now() + (accessToken.ttl * 1000);
-                  var expirationObj = null;
-                  if (userData.rememberme) {
-                    // allow cookie to be retained across browser sessions if "remember me" is checked
-                    expirationObj = { 
-                      expires:new Date(expiration) 
-                    }; 
-                  }
-                
-                  $cookies.putObject(Environment.getConfig('cookieName'), {
-                    id:accessToken.id,
-                    expiration:expiration
-                  }, expirationObj); 
-                }
-                
-                if (typeof options.loginRedirect !== "undefined" && options.loginRedirect === false) {
-                  currentUser = user;
-                  callback(false, user);
-                }
-                else {
-                  if (Environment.getConfig('loginRedirect') && 
-                  typeof Environment.getConfig('loginRedirect') === "function") {
-                    options.loginRedirect = Environment.getConfig('loginRedirect')(user, $cookies.getObject(Environment.getConfig('cookieName') + '_loginref'));
-                    // delete loginref so that it is only used once
-                    $cookies.remove(Environment.getConfig('cookieName') + '_loginref');
-                  }
-
-                  currentUser = user;
-
-                  //callback(false, user);
-
-                  window.location = options.loginRedirect || Environment.getConfig('loginRedirect');  
-                }
-                
+                self.initLogin(accessToken, userData, callback, options, user);
               },
               function (res) {
                 callback(res);
